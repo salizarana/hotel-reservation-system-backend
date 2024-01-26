8bc1947581ae5ceaff333585c7571dc539a4cafc
salizarana <salizarana@gmail.com>
2024-01-26 23:40:24 +0545

    Add project files.

---
+++ HotelReservationSystem/Models/Room.cs
@@ -0,0 +1,15 @@
+﻿namespace HotelReservationSystem.Models
+{
+    public class Room
+    {
+        public int Id { get; set; } 
+        public string? RoomNumber { get; set; }
+        public string? RoomType { get; set; }
+        public int Capacity { get; set; }
+        public float PricePerNight { get; set; }
+        public bool IsBooked { get; set; }
+        public string? Description { get; set; }
+        public string[]? Photos { get; set; } 
+        public string[]? Features { get; set; }
+    }
+}
---
+++ HotelReservationSystem/Models/RoomContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelReservationSystem.Models
+{
+    public class RoomContext: DbContext
+    {
+        public RoomContext(DbContextOptions<RoomContext> options) : base(options) { }
+
+        public DbSet<Room> Rooms { get; set; } = null!;
+    }
+}
